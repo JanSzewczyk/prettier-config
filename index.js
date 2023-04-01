a26f4c424aab145b3c7a0ab10bd64909567e117d
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-04-01 20:40:27 +0200

    feat(config): add `@trivago/prettier-plugin-sort-imports` library to shared config

--- index.js
+++ index.js
@@ -8,5 +8,21 @@ module.exports = {
   bracketSpacing: true,
   bracketSameLine: false,
   printWidth: 100,
-  plugins: ["prettier-plugin-packagejson"]
+
+  // 'prettier-plugin-sort-imports' configuration
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderGroupNamespaceSpecifiers: true,
+  importOrderCaseInsensitive: true,
+  importOrder: [
+    "^react$",
+    "^next($|/.*)",
+    "<THIRD_PARTY_MODULES>",
+    "^@(components|api|types|lib)(.*)$",
+    "^./(.*)",
+    "^../(.*)",
+    "(.scss|.css)$"
+  ],
+
+  plugins: ["prettier-plugin-packagejson", "@trivago/prettier-plugin-sort-imports"]
 };
