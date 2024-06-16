fece4987e870b757de66fe57a6cba1ed61d481c8
JanSzewczyk <jan.szewczyk1997@gmail.com>
2024-06-16 23:40:56 +0200

    fix(deps): bump deps

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/** @type {import('prettier').Options} */
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 module.exports = {
   semi: true,
   useTabs: false,
