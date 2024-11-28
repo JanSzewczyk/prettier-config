8b4d8ef6d0212d78e76fa5a2f6855adccf021ff1
JanSzewczyk <jan.szewczyk1997@gmail.com>
2024-11-28 22:48:44 +0100

    feat(config): update prettier configuration

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
-module.exports = {
+const has = (pkg) => {
+  try {
+    import.meta.resolve(pkg, import.meta.url);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+const hasTailwindcss = has("tailwindcss");
+
+/** @type {import('prettier').Config} */
+const baseConfig = {
   semi: true,
   useTabs: false,
   tabWidth: 2,
@@ -8,8 +19,22 @@ module.exports = {
   bracketSpacing: true,
   bracketSameLine: false,
   printWidth: 120,
-
-  tailwindFunctions: ["cva", "clsx"],
-
-  plugins: ["prettier-plugin-packagejson", "prettier-plugin-tailwindcss"]
+  proseWrap: "always",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  htmlWhitespaceSensitivity: "css",
+  embeddedLanguageFormatting: "auto"
 };
+
+/** @type {Array<string>} */
+const plugins = ["prettier-plugin-packagejson", !hasTailwindcss ? "prettier-plugin-tailwindcss" : null].filter(Boolean);
+
+/** @type {import('prettier-plugin-tailwindcss').PluginOptions} */
+const tailwindcssConfig = {
+  tailwindAttributes: ["class", "className", "ngClass", ".*[cC]lassName"],
+  tailwindFunctions: ["classNames", "clsx", "cn"]
+};
+
+export const config = { ...baseConfig, plugins, ...(!hasTailwindcss ? tailwindcssConfig : {}) };
+
+export default config;
